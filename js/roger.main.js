576b429f5f62f6335d63af900e1a9d5147b27652
Pabrick <pabrick.bastaky@gmail.com>
2018-04-27 23:02:54 +0200

    WIP: let's back to this someday

---
+++ js/roger.main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import RogerClock from './class/RogerClock.class.js';
+import RogerSprite from './class/RogerSprite.class.js';
+import RogerSheet from './class/RogerSheet.class.js';
+import RogerRegularSheet from './class/RogerRegularSheet.class.js';
+import RogerAnimation from './class/RogerAnimation.class.js';
+import RogerObject from './class/RogerObject.class.js';
