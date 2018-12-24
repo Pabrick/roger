38a626c3980ff8903eaa992d11931e6e0cfb0ff7
Pabrick <pabrick.bastaky@gmail.com>
2018-12-24 19:49:31 +0100

    *FIX: merge

--- src/main.js
+++ src/main.js
@@ -6,45 +6,42 @@ import RogerRegularSheet from './class/RogerRegularSheet.js';
 import RogerAnimation from './class/RogerAnimation.js';
 import RogerObject from './class/RogerObject.js';
 */
-const ssPablosHeads = new RogerRegularSheet('img/damn_pablos_heads.png', {w:512,h:512}, {w:54,h:90});
-const pabloIdleSprite = new RogerSprite('img/damn_pablos_heads.png', {w:54,h:90}, {x:0,y:0});
+const ss_horse2dragon = new RogerRegularSheet("img/ss_horse2dragon.png", {w:1150,h:1150}, {w:230,h:230});
 
-const animPablos_blink1 = new RogerAnimation('blink1',
-                                                ssPablosHeads,
-                                                [0,1,2,1,0],
-                                                { delay: 10, loops: -1, callBack: ()=> {
-                                                        console.log('blink complete!');
-                                                    }
-                                                });
+const anim_horse2dragon = new RogerAnimation("horse2dragon",
+                                                ss_horse2dragon,
+                                                [0,1,2,3,4,5,6,7,8],
+                                                { delay: 0, loops: 0, callBack: ()=>console.log("I'm a dragon!") });
 
-const animPablos_talk1 = new RogerAnimation('talk1',
-                                               ssPablosHeads,
-                                               [3,4,5,6,7],
-                                               { direction: 'random' });
+const anim_dragon2horse = new RogerAnimation("dragon2horse",
+                                                ss_horse2dragon,
+                                                [8,7,6,5,4,3,2,1,0],
+                                                { delay: 0, loops: 0, callBack: ()=>console.log("I'm a horse!") });
+ 
+const anim_horse2dragon2horse = new RogerAnimation("horse2dragon2horse",
+                                                ss_horse2dragon,
+                                                [0,1,2,3,4,5,6,7,8,7,6,5,4,3,2,1],
+                                                { delay: 10, loops: -1 });
 
-const animPablos_blink2 = new RogerAnimation('blink2', 
-                                                ssPablosHeads,
-                                                [9,10,11,10,9],
-                                                { delay: 10, callBack: ()=> {
-                                                        console.log('blink2 complete!');
-                                                    }
-                                                });
-                                                
-const animPablos_talk2 = new RogerAnimation('talk2', 
-                                               ssPablosHeads,
-                                               [12,13,14,15,16],
-                                               { callBack: ()=>console.log('talk2 complete!') });
+const anim_dragonOpenMouth = new RogerAnimation("dragonOpenMouth",
+                                                ss_horse2dragon,
+                                                [10,11,12,13,14,15,16,17,18],
+                                                { delay: 0, loops: 0, callBack: ()=>console.log("I'm a horse again!") });
 
-const pabloObject = new RogerObject('pablo', pabloIdleSprite);
+const anim_dragonCloseMouth = new RogerAnimation("dragonCloseMouth",
+                                                ss_horse2dragon,
+                                                [18,17,16,15,14,13,12,11,10],
+                                                { delay: 0, loops: 0, callBack: ()=>console.log("I'm a dragon again!") });
 
-pabloObject.addAnimation(animPablos_blink1);
-pabloObject.addAnimation(animPablos_blink2);
-pabloObject.addAnimation(animPablos_talk1);
-pabloObject.addAnimation(animPablos_talk2);
-pabloObject.playAnimation('blink1');
 
+const horseObject = new RogerObject("horse");
+horseObject.addAnimation(anim_horse2dragon);
+horseObject.addAnimation(anim_dragon2horse);
+horseObject.addAnimation(anim_horse2dragon2horse);
+horseObject.addAnimation(anim_dragonOpenMouth);
+horseObject.addAnimation(anim_dragonCloseMouth);
+horseObject.playAnimation("horse2dragon2horse");
 
 const rClock = new RogerClock(0.1);
-rClock.addObjectToUpdate(pabloObject);
-
+rClock.addObjectToUpdate(horseObject);
 rClock.start();
