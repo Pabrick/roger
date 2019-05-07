8237209d68fc75374d4ea7759c56850846be4fb1
Pabrick <pabrick.bastaky@gmail.com>
2019-05-07 21:00:46 +0100

    WIP

---
+++ src/js/roger.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import RogerClock from './lib/RogerClock.js';
+
+module.exports = {
+    Clock: RogerClock
+}
+
+/*
+module.exports = {
+    Clock: RogerClock,
+    Sprite: require('./lib/RogerSprite.js'),
+    Sheet: require('./lib/RogerSheet.js'),
+    RegularSheet: require('./lib/RogerRegularSheet.js'),
+    Animation: require('./lib/RogerAnimation.js'),
+    Object: require('./lib/RogerObject.js')
+}
+*/
